d68f0d6d8033b7c6ba78ca6fda35237b2fa2900d
minbbb <maks4339@gmail.com>
2019-04-09 21:21:28 +0300

    Spelling сorrections

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import random
 
 updater = Updater(token='TOKEN', use_context=True)
 
-variationsRU = ("{} бежит за печенюхами", "{} ковыляет за печеньем", "{} стремительно мчится за кукисами", "{} в слоумо уносится за печенбками", "Собирая своё снаряжение {} уходит в пустоши сражатся с крысами мутантами за крышки, на которые он купит печенье")
+variationsRU = ("{} бежит за печенюхами", "{} ковыляет за печеньем", "{} стремительно мчится за кукисами", "{} в слоумо уносится за печенбками", "Собирая своё снаряжение {} уходит в пустоши сражаться с крысами мутантами за крышки, на которые он купит печенье")
 variationsEN = ("{} running for cookies", "{} goes in slow motion for cookies")
 
 def start(update, context):
 	if update.message.from_user.language_code == "ru":
-		context.bot.send_message(chat_id=update.message.chat_id, text="Отправь мне имена кандидатов на поход за печеньками через пробел.\nИ я скажу кто пойдёт за печеньем))0)\nЛибо можещь добавить меня в группу и делать тоже самое добавляя в начале /cookie (пример - /cookie имя1 имя2). Или же использовать меня в inline режиме, для этого в любом чате набери @Who_will_go_for_cookies_bot и имена через пробел.")
+		context.bot.send_message(chat_id=update.message.chat_id, text="Отправь мне имена кандидатов на поход за печеньками через пробел.\nИ я скажу кто пойдёт за печеньем))0)\nЛибо можешь добавить меня в группу и делать тоже самое добавляя в начале /cookie (пример - /cookie имя1 имя2). Или же использовать меня в inline режиме, для этого в любом чате набери @Who_will_go_for_cookies_bot и имена через пробел.")
 	else:
 		context.bot.send_message(chat_id=update.message.chat_id, text="Send me the names of the candidates for the campaign for cookies through the space.\nAnd I will tell you who will go for cookies))0)\nOr you can add me to the group and do the same thing adding at the beginning /cookie (example - /cooke name1 name2). Or use me in inline mode, for this in any chat type @Who_will_go_for_cookies_bot and the names are separated by a space.")
 
